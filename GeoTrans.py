a9346a70c4944ab9d90423c633265383da3735f2
ShadyF <shadster09@gmail.com>
2016-05-04 23:37:42 +0200

    initial commit with rotation and translation done

---
+++ GeoTrans.py
@@ -0,0 +1,64 @@
+import config
+import PIL.Image
+from PIL import ImageTk
+#import numpy as np
+
+
+def rotate(canvas, left):
+    # Get current image and its dimensions
+    curr = config.current_image.load()
+    x_size = config.current_image.size[0]
+    y_size = config.current_image.size[1]
+
+    # Created a new flipped image
+    rotated_img = PIL.Image.new('RGB', (y_size, x_size))
+    # rotation = np.matrix(( (0, -1), (1, 0) ))
+
+    # Calculate center of rotation
+    cx = int(x_size / 2)
+    cy = int(y_size / 2)
+
+    # Calculate x and y offset of image in current orientation, used to bring
+    # pixels back to (0,0) in the newly created flipped image
+    min_x = min((0 - cy) + cx, (y_size - cy) + cx)
+    min_y = min(-(0 - cx) + cy, -(x_size - cx) + cy)
+
+    for x in range(x_size):
+        for y in range(y_size):
+            rgb_val = curr[x, y]        # get (R, G, B) tuple of current image pixel
+            x_new = (y - cy) + cx if left else -(y - cy) + cx      # cos(0.5pi) * (x - cx) - sin(0.5pi) * (y - cy) + cx
+            y_new = -(x - cx) + cy if left else (x - cx) + cy      # sin(0.5pi) * (x - cx) + cos(0.5pi) * (y - cy) + cy
+
+            # This part is extremely slow
+            if y_new-min_y in range(x_size) and x_new-min_x in range(y_size):
+                rotated_img.putpixel((x_new-min_x, y_new-min_y), rgb_val)
+
+    tk_img = ImageTk.PhotoImage(rotated_img)
+    canvas.create_image(400, 300, image=tk_img)     # (xpos, ypos, imgsrc)
+    canvas.image = tk_img       # Reference for Python's GC
+    config.current_image = rotated_img      # Keep reference of current image
+
+
+def translate(canvas, xx, yy, wrap_around):
+    curr = config.current_image.load()
+    x_size = config.current_image.size[0]
+    y_size = config.current_image.size[1]
+
+    new_img = PIL.Image.new('RGB', (x_size, y_size))
+
+    for x in range(x_size):
+        for y in range(y_size):
+            rgb_val = curr[x, y]
+            x_new = x + xx if not wrap_around else (x + xx) % x_size
+            y_new = y + yy if not wrap_around else (y + yy) % y_size
+
+            if x_new in range(x_size) and y_new in range(y_size):
+                new_img.putpixel((x_new, y_new), rgb_val)
+
+    tk_img = ImageTk.PhotoImage(new_img)
+    canvas.create_image(400, 300, image=tk_img)     # (xpos, ypos, imgsrc)
+    canvas.image = tk_img       # Reference for Python's GC
+    config.current_image = new_img      # Keep reference of current image
+
+def scale(canvas):
+    pass
---
+++ Theia.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from GeoTrans import *
+import config
+
+def loadImage():
+    filename = filedialog.askopenfilename()
+
+    img = PIL.Image.open(filename)
+    tk_img = ImageTk.PhotoImage(img)
+    canvas.create_image(400, 300, image=tk_img)     # (xpos, ypos, imgsrc)
+    canvas.image = tk_img		# Keep reference to PhotoImage so Python's garbage collector
+                                # does not get rid of it making the image dissapear
+    config.current_image = img
+
+root = Tk()
+root.title("Theia")
+
+mainframe = ttk.Frame(root,padding="3 3 12 12")
+mainframe.grid(row=0, column=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+
+
+canvas = Canvas(mainframe, width=800, height=600)
+canvas.grid(row=0, column=0, rowspan=3)		# put in row 0 col 0 and span 2 rows
+canvas.columnconfigure(0, weight=3)
+buttons_frame = ttk.Frame(mainframe)
+buttons_frame.grid(row=0, column=1, sticky=N)
+
+load_image_button = ttk.Button(buttons_frame, text="Load Image...", command=loadImage)
+load_image_button.grid(row=0, column=1, sticky=N+W+E, columnspan=2, pady=10)
+
+#####################
+# Rotation Section #
+#####################
+
+rotate_button_label = ttk.Label(buttons_frame, text="Rotation").grid(row=1, column=1, columnspan = 2, sticky=N, pady=8)
+
+rotate_left_button = ttk.Button(buttons_frame, text="Rotate Left", command=lambda: rotate(canvas, True))
+rotate_left_button.grid(row=2, column=1, sticky=N, padx=5)
+
+rotate_right_button = ttk.Button(buttons_frame, text="Rotate Right", command=lambda: rotate(canvas, False))
+rotate_right_button.grid(row=2, column=2, sticky=N, padx=5)
+ttk.Separator(buttons_frame, orient=HORIZONTAL).grid(row=3, columnspan=3, sticky=(W, E),pady=8)
+
+#######################
+# Translation Section #
+#######################
+
+translation_button_label = ttk.Label(buttons_frame, text="Translation").grid(row=4, column=1, columnspan = 2)
+
+delta_x_label = ttk.Label(buttons_frame, text="X dir.").grid(row=5, column=1)
+delta_y_label = ttk.Label(buttons_frame, text="Y dir.").grid(row=5, column=2)
+
+delta_x_entry = ttk.Entry(buttons_frame, width=9)
+delta_x_entry.grid(row=6, column=1)
+delta_y_entry = ttk.Entry(buttons_frame, width=9)
+delta_y_entry.grid(row=6, column=2)
+
+wrap_around = BooleanVar()
+wrap_around_check = ttk.Checkbutton(buttons_frame, text="Wrap around", variable=wrap_around)
+wrap_around_check.grid(row=7, column=1, columnspan=2)
+
+translation_button = ttk.Button(buttons_frame, text="Translate", command=lambda: translate(canvas, int(delta_x_entry.get()),
+                                                                                           int(delta_y_entry.get()), wrap_around.get()))
+translation_button.grid(row=8, column=1, columnspan=2, sticky=W+E, pady=5)
+
+ttk.Separator(buttons_frame, orient=HORIZONTAL).grid(row=9, columnspan=3, sticky=(W, E),pady=8)
+
+###################
+# Scaling Section #
+###################
+
+root.mainloop()
